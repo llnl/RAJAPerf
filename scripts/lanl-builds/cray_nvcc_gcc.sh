0c4d6b4b9e11048d7e9f7a810b5312dcd3075e85
guthrey1 <guthrey1@llnl.gov>
2025-01-02 18:20:48 -0700

    Venado build

---
+++ scripts/lanl-builds/cray_nvcc_gcc.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Copyright (c) 2017-24, Lawrence Livermore National Security, LLC
+# and RAJA project contributors. See the RAJAPerf/LICENSE file for details.
+#
+# SPDX-License-Identifier: (BSD-3-Clause)
+###############################################################################
+
+if [[ $# -lt 3 ]]; then
+  echo
+  echo "You must pass 3 arguments to the script (in this order): "
+  echo "   1) compiler version number for nvcc"
+  echo "   2) CUDA compute architecture (number only, not 'sm_90' for example)"
+  echo "   3) compiler version number for gcc"
+  echo
+  echo "For example: "
+  echo "    cray_nvcc_gcc.sh 12.5 90 12.3.0"
+  exit
+fi
+
+COMP_NVCC_VER=$1
+COMP_ARCH=$2
+COMP_GCC_VER=$3
+shift 3
+
+BUILD_SUFFIX=lanl_cray-nvcc-${COMP_NVCC_VER}-${COMP_ARCH}-gcc-${COMP_GCC_VER}
+RAJA_HOSTCONFIG=../tpl/RAJA/host-configs/lanl-builds/nvcc_gcc_X.cmake
+
+echo
+echo "Creating build directory build_${BUILD_SUFFIX} and generating configuration in it"
+echo "Configuration extra arguments:"
+echo "   $@"
+echo
+
+rm -rf build_${BUILD_SUFFIX} >/dev/null
+mkdir build_${BUILD_SUFFIX} && cd build_${BUILD_SUFFIX}
+
+module load cmake/3.29.6
+
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -C ${RAJA_HOSTCONFIG} \
+  -DRAJA_PERFSUITE_ENABLE_TESTS=Off \
+  -DRAJA_ENABLE_TESTS=Off \
+  -DRAJA_ENABLE_EXAMPLES=Off \
+  -DCMAKE_INSTALL_PREFIX=../install_${BUILD_SUFFIX} \
+  "$@" \
+  ..
+
+echo
+echo "***********************************************************************"
+echo
+echo "cd into directory build_${BUILD_SUFFIX} and run make to build RAJA Perf Suite"
+echo
+echo "  Please note that you have to disable CUDA GPU hooks when you run"
+echo "  the RAJA Perf Suite; for example,"
+echo
+echo "    ./bin/raja-perf.exe"
+echo
+echo "***********************************************************************"
