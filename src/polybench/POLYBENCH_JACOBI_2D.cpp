a586e4733958971fc3e1542f2f3a0ca816f7543f
Michael McKinsey <michaelmckinsey1@gmail.com>
2025-08-18 14:01:37 -0700

    Ignore tsteps

--- src/polybench/POLYBENCH_JACOBI_2D.cpp
+++ src/polybench/POLYBENCH_JACOBI_2D.cpp
@@ -50,7 +50,7 @@ POLYBENCH_JACOBI_2D::POLYBENCH_JACOBI_2D(const RunParams& params)
 
   setComplexity(Complexity::N);
 
-  setMaxLoopDimensions(3);
+  setMaxLoopDimensions(2); // Do not count TSTEPS as it is not a part of the kernel
   setMaxArrayDimensions(2);
   setNumArrays(2);
 
