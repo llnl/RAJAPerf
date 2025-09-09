ce5a5b1b602059f4117e5f0a283eb76850b9ebde
Jeffrey Grandy <grandy1@llnl.gov>
2025-09-09 10:08:54 -0700

    Preliminary Sycl implementation for INTSC_HEXRECT.
    This is untested and considered to be a placeholder until we
    debug and test it on a Sycl platform.

--- src/apps/INTSC_HEXRECT-Sycl.cpp
+++ src/apps/INTSC_HEXRECT-Sycl.cpp
@@ -31,17 +31,8 @@ template < size_t work_group_size >
 void INTSC_HEXRECT::runSyclVariantImpl(VariantID vid)
 {
   const Index_type run_reps = getRunReps();
-  const Index_type ibegin = 0 ;
-  const Index_type iend     = m_tri_per_intsc * getActualProblemSize() ;
-
-  const Size_type  n_subz_intsc = 8 * getActualProblemSize() ;
-  const Size_type  nisc_stage   = n_subz_intsc ;
-
-  const Size_type  n_szgrp     = ( n_subz_intsc + 7 ) / 8 ;
-  const size_t     gsize_fixup = RAJA_DIVIDE_CEILING_INT(n_szgrp, work_group_size) ;
-  const Index_type iend_fixup  = gsize_fixup * work_group_size ;
-
-  const Size_type  n_szpairs   = n_subz_intsc ;
+  const Index_type ibegin   = 0 ;
+  const Index_type iend     = m_nrecords ;
 
   auto res{getSyclResource()};
   auto qu = res.get_queue();
@@ -61,21 +52,15 @@ void INTSC_HEXRECT::runSyclVariantImpl(VariantID vid)
 
           Index_type i = item.get_global_id(0) + ibegin;
 
-          long ith     = i ;
+          long irec     = i ;
           long blksize = work_group_size ;
           long blk     = i / blksize ;
-          double *vv_out = (double*) vv_int + 8*blk ;
+
+          int const max_polygon_pts = 10 ;
+          double xd_work[ (3 * max_polygon_pts+1) ] ;
+          double *my_qx = xd_work ;
+
           INTSC_HEXRECT_BODY;
-        });
-      });
-
-      qu->submit([&] (sycl::handler& h) {
-        h.parallel_for(sycl::nd_range<1>(gsize_fixup, work_group_size),
-                       [=] (sycl::nd_item<1> item ) {
-
-          Index_type i = item.get_global_id(0) + ibegin;
-          int ith     = i ;
-          FIXUP_VV_BODY ;
         });
       });
 
@@ -90,18 +75,18 @@ void INTSC_HEXRECT::runSyclVariantImpl(VariantID vid)
       RAJA::forall< RAJA::sycl_exec<work_group_size, true /*async*/> >( res,
         RAJA::RangeSegment(ibegin, iend), [=] (Index_type i) {
 
-        long ith     = i ;
-        long blksize = work_group_size ;
-        long blk     = i / blksize ;
-        double *vv_out = (double*) vv_int + 8*blk ;
-        INTSC_HEXRECT_BODY;
-      });
+          Index_type i = item.get_global_id(0) + ibegin;
 
-      RAJA::forall< RAJA::sycl_exec<work_group_size, true /*async*/> >( res,
-        RAJA::RangeSegment(ibegin, iend_fixup), [=] (Index_type i) {
-        int ith     = i ;
-        FIXUP_VV_BODY ;
-        }
+          long irec     = i ;
+          long blksize = work_group_size ;
+          long blk     = i / blksize ;
+
+          int const max_polygon_pts = 10 ;
+          double xd_work[ (3 * max_polygon_pts+1) ] ;
+          double *my_qx = xd_work ;
+
+          INTSC_HEXRECT_BODY;
+      });
 
     }
     stopTimer();
@@ -111,7 +96,7 @@ void INTSC_HEXRECT::runSyclVariantImpl(VariantID vid)
   }
 }
 
-RAJAPERF_GPU_BLOCK_SIZE_TUNING_DEFINE_BOILERPLATE(INTSC_HEXHEX, Sycl)
+RAJAPERF_GPU_BLOCK_SIZE_TUNING_DEFINE_BOILERPLATE(INTSC_HEXRECT, Sycl)
 
 } // end namespace apps
 } // end namespace rajaperf
