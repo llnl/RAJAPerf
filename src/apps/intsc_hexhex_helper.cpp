0182b50e6e7149d390ab064faea988c2d82d9d8c
Jeffrey Grandy <grandy1@llnl.gov>
2025-09-15 16:30:14 -0700

    Moved inferior kernel functions for INTSC_ to auxiliary header files.
    Kernel functions for INTSC_HEXHEX and INTSC_HEXRECT.

---
+++ src/apps/intsc_hexhex_helper.cpp
@@ -0,0 +1,399 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Copyright (c) 2017-25, Lawrence Livermore National Security, LLC
+// and RAJA Performance Suite project contributors.
+// See the RAJAPerf/LICENSE file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+#ifndef RAJAPerf_Apps_INTSC_HEXHEX_HELPER_HPP
+#define RAJAPerf_Apps_INTSC_HEXHEX_HELPER_HPP
+
+
+RAJA_HOST_DEVICE
+RAJA_INLINE void clip_polygon_ge_0
+    ( double *cin,   // the cut coordinate, can be xin, yin, or zin.
+      double *xin, double *yin, double *zin, double *hin, // input coordinates
+      int &first, int &avail, int *next )   // linked list
+{
+  int j  = first ;
+
+  int first0 = first ;
+  int j1 = -1, j2 = -1 ;
+  int jj1 = -1, jj2 = -1 ;
+
+  double c0 = ( j >= 0 ) ? cin[j] : 0.0 ;
+  double c00 = c0 ;
+  double clast = c0 ;
+
+  while ( j >= 0 ) {
+    int jj = next[j] ;
+    int jp = jj ;       // advancing, jp is -1 at end.
+    if ( jj < 0 ) { jj = first0 ; }   // last edge of polygon
+
+    double c1 = cin[jj] ;
+    if ( ( c0 >= 0 ) and ( c1 < 0 ) ) {
+      j1 = j ;
+      jj1 = jj ;
+    }
+    if ( ( c0 < 0 ) and ( c1 >= 0 ) ) {
+      j2 = j ;
+      jj2 = jj ;
+    }
+    j = jp ;
+    clast = c0 ;
+    c0 = c1 ;
+  }
+
+  int jr1=-1, jr2=-1 ;
+
+  if ( j1 >= 0 ) {   // Insert first crossover point
+
+    jr1 = avail ;
+    avail = next[avail] ;
+    double eta = ( 0.0 - cin[jj1] ) / ( cin[j1] - cin[jj1] ) ;
+    xin[jr1] = xin[j1] * eta + xin[jj1] * ( 1.0 - eta ) ;
+    yin[jr1] = yin[j1] * eta + yin[jj1] * ( 1.0 - eta ) ;
+    zin[jr1] = zin[j1] * eta + zin[jj1] * ( 1.0 - eta ) ;
+    hin[jr1] = hin[j1] * eta + hin[jj1] * ( 1.0 - eta ) ;
+
+    jr2 = avail ;      // Insert second crossover point
+    avail = next[avail] ;
+    eta = ( 0.0 - cin[j2] ) / ( cin[jj2] - cin[j2] ) ;
+    xin[jr2] = xin[jj2] * eta + xin[j2] * ( 1.0 - eta ) ;
+    yin[jr2] = yin[jj2] * eta + yin[j2] * ( 1.0 - eta ) ;
+    zin[jr2] = zin[jj2] * eta + zin[j2] * ( 1.0 - eta ) ;
+    hin[jr2] = hin[jj2] * eta + hin[j2] * ( 1.0 - eta ) ;
+  }
+
+  first = -1 ;
+
+  j = first0 ;
+  while ( j >= 0 ) {   // Make removed points available.
+    int jp = next[j] ;
+    if ( cin[j] < 0.0 ) {
+      next[j] = avail ;
+      avail = j ;
+    } else if ( first == -1 ) {
+      first = j ;        // Set first point for output polygon.
+    }
+    j = jp ;
+  }
+
+
+  if ( j1 >= 0 ) {     // Set linked list for crossover points.
+    next[j1] = jr1 ;
+    next[jr1] = jr2 ;
+    next[jr2] = ( ( clast < 0 ) or ( c00 < 0 ) ) ? -1 : jj2 ;
+  }
+  return ;
+}
+
+
+
+//   Simplified volume calculation, for the area under one
+//   polygonal face, on the Cuda device.
+//   Planar polygon.
+//   Compute volume, moments between polygon and the z=0 plane.
+RAJA_HOST_DEVICE
+RAJA_INLINE void cuda_hex_volpolyh_1poly
+    ( double *x, double *y, double *z,
+      int const first,
+      int const *next,
+      double &vv,
+      double &vx,
+      double &vy,
+      double &vz )
+{
+  if ( first < 0 ) { return ; }   // No polygon remains after clipping.
+
+  int j0 = first ;
+
+  double x0  = x[j0] ;
+  double y0  = y[j0] ;
+  double z0  = z[j0] ;
+
+  int j1 = next[j0] ;
+
+  double x1  = x[j1] ;
+  double y1  = y[j1] ;
+  double z1  = z[j1] ;
+  double dx1 = x1 - x0 ;
+  double dy1 = y1 - y0 ;
+
+  int j2 = next[j1] ;
+
+  while ( j2 >= 0 ) {   // Vertices
+
+    double x2  = x[j2] ;
+    double y2  = y[j2] ;
+    double z2  = z[j2] ;
+    double dx2 = x2 - x0 ;
+    double dy2 = y2 - y0 ;
+
+    double area2 = (dx1 * dy2 - dx2 * dy1) ;
+    double v0 = ( z0 + z1 + z2 ) * area2 ;
+    vv += v0 ;
+    vx += v0 * ( x0 + x1 + x2 ) + area2 * ( x0 * z0 + x1 * z1 + x2 * z2 );
+    vy += v0 * ( y0 + y1 + y2 ) + area2 * ( y0 * z0 + y1 * z1 + y2 * z2 );
+    vz += ( z0 * z0 + z1 * z1 + z2 * z2 + z0 * z1 + z0 * z2 + z1 * z2 )
+        * area2 ;
+
+    x1=x2 ;   y1=y2 ;   z1=z2 ;    // Rotate.
+    dx1=dx2 ; dy1=dy2 ;
+
+    j2 = next[j2] ;
+  }
+}
+
+
+
+RAJA_HOST_DEVICE
+RAJA_INLINE void cuda_intsc_tri_tet
+    ( double const xdt[3],    // donor triangle coordinates
+      double const ydt[3],
+      double const zdt[3],
+      double xtt[4],    // target tet coordinates (modified here)
+      double ytt[4],
+      double ztt[4],
+      double &vv_thr,     // volume contribution for this triangle-tet
+      double &vx_thr,     // x moment contribution for this triangle-tet
+      double &vy_thr,     // y moment contribution for this triangle-tet
+      double &vz_thr )    // z moment contribution for this triangle-tet
+{
+  double det, deti ;
+  double ha[9] ;      // 1 - x - y - z
+
+  double xa[9], ya[9], za[9], h2[10] ;
+  int *next1 = (int*) h2 ;
+  int *next  = next1 + 10 ;
+
+  double vv = 0.0, vx = 0.0, vy = 0.0, vz = 0.0 ;  // volume, moments.
+
+  xtt[1] -= xtt[0] ;
+  xtt[2] -= xtt[0] ;
+  xtt[3] -= xtt[0] ;
+  ytt[1] -= ytt[0] ;
+  ytt[2] -= ytt[0] ;
+  ytt[3] -= ytt[0] ;
+  ztt[1] -= ztt[0] ;
+  ztt[2] -= ztt[0] ;
+  ztt[3] -= ztt[0] ;
+
+  det =
+      xtt[1]  *  ytt[2]  *  ztt[3]  - xtt[1]  *  ytt[3]  *  ztt[2]  +
+      xtt[2]  *  ytt[3]  *  ztt[1]  - xtt[2]  *  ytt[1]  *  ztt[3]  +
+      xtt[3]  *  ytt[1]  *  ztt[2]  - xtt[3]  *  ytt[2]  *  ztt[1]  ;
+  deti = det / ( det*det + 1.0e-100 ) ;
+
+  // Cross products.
+  double cyz = ytt[2] * ztt[3] - ztt[2] * ytt[3] ;
+  double czx = ztt[2] * xtt[3] - xtt[2] * ztt[3] ;
+  double cxy = xtt[2] * ytt[3] - ytt[2] * xtt[3] ;
+
+  //   Coordinates of the facet in the transformed frame.
+  xa[0] = (xdt[0] - xtt[0]) * cyz + (ydt[0] - ytt[0]) * czx +
+      (zdt[0] - ztt[0]) * cxy ;
+  xa[1] = (xdt[1] - xtt[0]) * cyz + (ydt[1] - ytt[0]) * czx +
+      (zdt[1] - ztt[0]) * cxy ;
+  xa[2] = (xdt[2] - xtt[0]) * cyz + (ydt[2] - ytt[0]) * czx +
+      (zdt[2] - ztt[0]) * cxy ;
+
+  cyz = ytt[3] * ztt[1] - ztt[3] * ytt[1] ;
+  czx = ztt[3] * xtt[1] - xtt[3] * ztt[1] ;
+  cxy = xtt[3] * ytt[1] - ytt[3] * xtt[1] ;
+
+  ya[0] = (xdt[0] - xtt[0]) * cyz + (ydt[0] - ytt[0]) * czx +
+      (zdt[0] - ztt[0]) * cxy ;
+  ya[1] = (xdt[1] - xtt[0]) * cyz + (ydt[1] - ytt[0]) * czx +
+      (zdt[1] - ztt[0]) * cxy ;
+  ya[2] = (xdt[2] - xtt[0]) * cyz + (ydt[2] - ytt[0]) * czx +
+      (zdt[2] - ztt[0]) * cxy ;
+
+  cyz = ytt[1] * ztt[2] - ztt[1] * ytt[2] ;
+  czx = ztt[1] * xtt[2] - xtt[1] * ztt[2] ;
+  cxy = xtt[1] * ytt[2] - ytt[1] * xtt[2] ;
+
+  za[0] = (xdt[0] - xtt[0]) * cyz + (ydt[0] - ytt[0]) * czx +
+      (zdt[0] - ztt[0]) * cxy ;
+  za[1] = (xdt[1] - xtt[0]) * cyz + (ydt[1] - ytt[0]) * czx +
+      (zdt[1] - ztt[0]) * cxy ;
+  za[2] = (xdt[2] - xtt[0]) * cyz + (ydt[2] - ytt[0]) * czx +
+      (zdt[2] - ztt[0]) * cxy ;
+
+  xa[0] *= deti ;    xa[1] *= deti ;    xa[2] *= deti ;
+  ya[0] *= deti ;    ya[1] *= deti ;    ya[2] *= deti ;
+  za[0] *= deti ;    za[1] *= deti ;    za[2] *= deti ;
+
+  //  Clip on h2 first.
+  ha[0] = 1.0 - xa[0] - ya[0] - za[0] ;
+  ha[1] = 1.0 - xa[1] - ya[1] - za[1] ;
+  ha[2] = 1.0 - xa[2] - ya[2] - za[2] ;
+  h2[0] = 1.0 - xa[0] - ya[0] ;
+  h2[1] = 1.0 - xa[1] - ya[1] ;
+  h2[2] = 1.0 - xa[2] - ya[2] ;
+
+  //  Initialize triangle and available slots.
+  next[0] = 1 ;   next[1] = 2 ;   next[2] = -1 ;
+  next[3] = 4 ;   next[4] = 5 ;   next[5] = 6 ;  next[6] = 7 ;
+  next[7] = 8 ;   next[8] = -1 ;
+
+  int first = 0 ;
+  int avail = 3 ;
+
+  clip_polygon_ge_0
+      ( h2, xa, ya, za, ha, first, avail, next ) ;
+
+  //  Clip on Cartesian faces of the unit tet.
+  clip_polygon_ge_0
+      ( xa, xa, ya, za, ha, first, avail, next ) ;
+
+  clip_polygon_ge_0
+      ( ya, xa, ya, za, ha, first, avail, next ) ;
+
+  clip_polygon_ge_0
+      ( za, xa, ya, za, ha, first, avail, next ) ;
+
+  int first1 = first, avail1 = avail;
+  for ( int k = 0 ; k < 9 ; ++k ) {
+    next1[k] = next[k] ;
+  }
+
+  //  Clip on h>=0
+
+  clip_polygon_ge_0
+      ( ha, xa, ya, za, ha, first, avail, next ) ;
+
+
+  cuda_hex_volpolyh_1poly( xa, ya, za, first, next, vv, vx, vy, vz ) ;
+
+
+  //  In dimensionless transformed coordinates, quantity smaller
+  // than machine epsilon is not significant.
+  int j = first1 ;
+  while ( j >= 0 ) {
+    ha[j] = -ha[j] - 1.0e-50 ;
+    j = next1[j] ;
+  }
+
+  // Clip on h<0
+  clip_polygon_ge_0
+      ( ha, xa, ya, za, ha, first1, avail1, next1 ) ;
+
+  //  project to unit tet.
+  j = first1 ;
+  while ( j >= 0 ) {
+    za[j] = 1.0 - xa[j] - ya[j] ;
+    j = next1[j] ;
+  }
+
+  cuda_hex_volpolyh_1poly( xa, ya, za, first1, next1, vv, vx, vy, vz ) ;
+
+  //  Volume, moments of the intersection in the unit tet frame.
+  vv *= 0.16666666666666667 ;
+  vx *= 0.041666666666666667 ;
+  vy *= 0.041666666666666667 ;
+  vz *= 0.041666666666666667 ;
+
+  //   Transform moments to the physical frame.
+  vx_thr += det * (xtt[0] * vv + xtt[1] * vx + xtt[2] * vy + xtt[3] * vz);
+  vy_thr += det * (ytt[0] * vv + ytt[1] * vx + ytt[2] * vy + ytt[3] * vz);
+  vz_thr += det * (ztt[0] * vv + ztt[1] * vx + ztt[2] * vy + ztt[3] * vz);
+
+  //   Transform intersection volume to the physical frame.
+  vv_thr += det * vv ;
+}
+
+
+
+//  Compute the contribution of a donor triangle and a target tet
+//         to intersection between hex subzones.
+//   Each subzone is twelve triangular facets (six tets).
+//
+RAJA_HOST_DEVICE
+RAJA_INLINE void hex_intsc_subz
+    ( double const *xds,    //  [24] donor subzone coords
+      double const *xts,    //  [24] target subzone coords
+      int const dfacet,     // which donor facet
+      int const ttet,       // which target tet
+      double &vv_thr,     // volume contribution for this triangle-tet
+      double &vx_thr,     // x moment contribution for this triangle-tet
+      double &vy_thr,     // y moment contribution for this triangle-tet
+      double &vz_thr )    // z moment contribution for this triangle-tet
+{
+  double const *yds = xds + 8 ;
+  double const *zds = yds + 8 ;
+
+  double const *yts = xts + 8 ;
+  double const *zts = yts + 8 ;
+
+  vv_thr = 0.0 ;
+  vx_thr = 0.0 ;
+  vy_thr = 0.0 ;
+  vz_thr = 0.0 ;
+
+  int const n_dfacets = 12 ;
+  int const len_cycnod = n_dfacets / 2 + 1 ;
+
+  //  coordinates of the donor triangle
+  double xdt[3], ydt[3], zdt[3] ;
+
+  do {
+    //  cyclic nodes to form facets with node 0.
+    int cyc_nod[len_cycnod] = { 1, 5, 4, 6, 2, 3, 1 } ;
+
+    // which subzone vertices form the triangular facet.
+    int v0, v1, v2 ;
+    if ( dfacet < 6 ) {
+      v0 = 0 ;
+      v1 = cyc_nod[dfacet] ;
+      v2 = cyc_nod[dfacet+1] ;
+    } else {
+      v0 = 7 ;
+      v1 = cyc_nod[n_dfacets-dfacet] ;
+      v2 = cyc_nod[n_dfacets-dfacet - 1] ;  // reverse order
+    }
+
+    //  Donor triangle coordinates.
+    xdt[0] = xds[v0] ;    // Donor facet vertices
+    xdt[1] = xds[v1] ;
+    xdt[2] = xds[v2] ;
+    ydt[0] = yds[v0] ;
+    ydt[1] = yds[v1] ;
+    ydt[2] = yds[v2] ;
+    zdt[0] = zds[v0] ;
+    zdt[1] = zds[v1] ;
+    zdt[2] = zds[v2] ;
+
+  } while ( false ) ;
+
+  //   Set up the target tet and do the intersections.
+
+  double xtt[4], ytt[4], ztt[4] ;
+
+  xtt[0] = xts[0] ;
+  ytt[0] = yts[0] ;
+  ztt[0] = zts[0] ;
+
+  //  subzone vertices that form the cycle for tets.
+  int vert_cyc[6] = { 1, 3, 2, 6, 4, 5 } ;
+
+  int v1 = vert_cyc[ttet] ;
+  xtt[1] = xts[v1] ;
+  ytt[1] = yts[v1] ;
+  ztt[1] = zts[v1] ;
+  int v2 = vert_cyc[(ttet+1)%6] ;
+  xtt[2] = xts[v2] ;
+  ytt[2] = yts[v2] ;
+  ztt[2] = zts[v2] ;
+  xtt[3] = xts[7] ;
+  ytt[3] = yts[7] ;
+  ztt[3] = zts[7] ;
+
+  cuda_intsc_tri_tet
+      ( xdt, ydt, zdt, xtt, ytt, ztt,
+        vv_thr, vx_thr, vy_thr, vz_thr ) ;
+}
+
+
+#endif // close include guard RAJAPerf_Apps_INTSC_HEXHEX_HELPER_HPP
