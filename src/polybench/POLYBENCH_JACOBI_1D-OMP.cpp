57e74d521a3e2b8c936ec45b789cf46b48a0a15a
Jason Burmark <burmark1@llnl.gov>
2025-09-26 14:32:05 -0700

    remove tsteps from JACOBI_1D

--- src/polybench/POLYBENCH_JACOBI_1D-OMP.cpp
+++ src/polybench/POLYBENCH_JACOBI_1D-OMP.cpp
@@ -41,17 +41,13 @@ void POLYBENCH_JACOBI_1D::runOpenMPVariant(VariantID vid, size_t RAJAPERF_UNUSED
       startTimer();
       for (RepIndex_type irep = 0; irep < run_reps; irep = irep + 1) {
 
-        for (Index_type t = 0; t < tsteps; ++t) {
-
-          #pragma omp parallel for
-          for (Index_type i = 1; i < N-1; ++i ) {
-            POLYBENCH_JACOBI_1D_BODY1;
-          }
-          #pragma omp parallel for
-          for (Index_type i = 1; i < N-1; ++i ) {
-            POLYBENCH_JACOBI_1D_BODY2;
-          }
-
+        #pragma omp parallel for
+        for (Index_type i = 1; i < N-1; ++i ) {
+          POLYBENCH_JACOBI_1D_BODY1;
+        }
+        #pragma omp parallel for
+        for (Index_type i = 1; i < N-1; ++i ) {
+          POLYBENCH_JACOBI_1D_BODY2;
         }
 
       }
@@ -65,17 +61,13 @@ void POLYBENCH_JACOBI_1D::runOpenMPVariant(VariantID vid, size_t RAJAPERF_UNUSED
       startTimer();
       for (RepIndex_type irep = 0; irep < run_reps; irep = irep + 1) {
 
-        for (Index_type t = 0; t < tsteps; ++t) {
-
-          #pragma omp parallel for
-          for (Index_type i = 1; i < N-1; ++i ) {
-            poly_jacobi1d_lam1(i);
-          }
-          #pragma omp parallel for
-          for (Index_type i = 1; i < N-1; ++i ) {
-            poly_jacobi1d_lam2(i);
-          }
-
+        #pragma omp parallel for
+        for (Index_type i = 1; i < N-1; ++i ) {
+          poly_jacobi1d_lam1(i);
+        }
+        #pragma omp parallel for
+        for (Index_type i = 1; i < N-1; ++i ) {
+          poly_jacobi1d_lam2(i);
         }
 
       }
@@ -91,19 +83,15 @@ void POLYBENCH_JACOBI_1D::runOpenMPVariant(VariantID vid, size_t RAJAPERF_UNUSED
       startTimer();
       for (RepIndex_type irep = 0; irep < run_reps; irep = irep + 1) {
 
-        for (Index_type t = 0; t < tsteps; ++t) {
+        RAJA::forall<RAJA::omp_parallel_for_exec>( res,
+          RAJA::RangeSegment{1, N-1},
+          poly_jacobi1d_lam1
+        );
 
-          RAJA::forall<RAJA::omp_parallel_for_exec>( res,
-            RAJA::RangeSegment{1, N-1},
-            poly_jacobi1d_lam1
-          );
-
-          RAJA::forall<RAJA::omp_parallel_for_exec>( res,
-            RAJA::RangeSegment{1, N-1},
-            poly_jacobi1d_lam2
-          );
-
-        }
+        RAJA::forall<RAJA::omp_parallel_for_exec>( res,
+          RAJA::RangeSegment{1, N-1},
+          poly_jacobi1d_lam2
+        );
 
       }
       stopTimer();
