fb7a51b06be15f83c541af8fabbd6615b095f780
Jeffrey Grandy <grandy1@llnl.gov>
2025-09-29 17:34:12 -0700

    Commented out the Sycl code in INTSC_HEXHEX.

--- src/apps/INTSC_HEXHEX-Sycl.cpp
+++ src/apps/INTSC_HEXHEX-Sycl.cpp
@@ -21,7 +21,7 @@
 #define WARPSIZE 32
 #define __shfl_xor_sync(mask,val,n) \
   sycl::permute_group_by_xor(item.get_sub_group(),val,n)
-#define __syncthreads sycl::group_barrier ( item.get_group() )
+#define __syncthreads item.get_group().barrier()
 
 namespace rajaperf
 {
@@ -33,60 +33,60 @@ template < Size_type work_group_size >
 void INTSC_HEXHEX::runSyclVariantImpl(VariantID vid)
 {
   const Index_type run_reps = getRunReps();
-  const Index_type ibegin = 0 ;
-  const Index_type iend     = m_tri_per_intsc * getActualProblemSize() ;
+  // const Index_type ibegin = 0 ;
+  // const Index_type iend     = m_tri_per_intsc * getActualProblemSize() ;
 
-  const Size_type  n_subz_intsc = 8 * getActualProblemSize() ;
-  const Size_type  nisc_stage   = n_subz_intsc ;
+  // const Size_type  n_subz_intsc = 8 * getActualProblemSize() ;
+  // const Size_type  nisc_stage   = n_subz_intsc ;
 
-  const Size_type  n_szgrp     = n_subz_intsc / 8 ;
+  // const Size_type  n_szgrp     = n_subz_intsc / 8 ;
 
-  const Size_type  gsize_fixup =
-      RAJA_DIVIDE_CEILING_INT(n_szgrp, work_group_size) ;
+  // const Size_type  gsize_fixup =
+  //     RAJA_DIVIDE_CEILING_INT(n_szgrp, work_group_size) ;
 
-  const Index_type iend_fixup  = gsize_fixup * work_group_size ;
+  // const Index_type iend_fixup  = gsize_fixup * work_group_size ;
 
-  const Size_type  n_szpairs   = n_subz_intsc ;
+  // const Size_type  n_szpairs   = n_subz_intsc ;
 
-  auto res{getSyclResource()};
-  auto qu = res.get_queue();
+  // auto res{getSyclResource()};
+  // auto qu = res.get_queue();
 
-  INTSC_HEXHEX_DATA_SETUP;
+  // INTSC_HEXHEX_DATA_SETUP;
 
   if ( vid == Base_SYCL ) {
 
     startTimer();
     for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
 
-      const Size_type global_size =
-          work_group_size * RAJA_DIVIDE_CEILING_INT(iend, work_group_size);
+      // const Size_type global_size =
+      //     work_group_size * RAJA_DIVIDE_CEILING_INT(iend, work_group_size);
 
-      qu->submit([&] (sycl::handler& h) {
-        h.parallel_for(sycl::nd_range<1>(global_size, work_group_size),
-                       [=] (sycl::nd_item<1> item ) {
+      // qu->submit([&] (sycl::handler& h) {
+      //   h.parallel_for(sycl::nd_range<1>(global_size, work_group_size),
+      //                  [&] (sycl::nd_item<1> item ) {
 
-          sycl::local_accessor<Real_type,1> vv_reduce(sycl::range<1>(16), h ) ;
+      //     sycl::local_accessor<Real_type,1> vv_reduce(sycl::range<1>(16), h ) ;
 
-          Index_type i = item.get_global_id(0) + ibegin;
+      //     Index_type i = item.get_global_id(0) + ibegin;
 
-          Index_type ith     = i ;
-          Index_type blksize = work_group_size ;
-          Index_type blk     = i / blksize ;
-          Index_type thridx    = i % work_group_size ;
-          Real_ptr vv_out = (Real_ptr) vv_int + 8*blk ;
-          INTSC_HEXHEX_BODY;
-        });
-      });
+      //     Index_type ith     = i ;
+      //     Index_type blksize = work_group_size ;
+      //     Index_type blk     = i / blksize ;
+      //     Index_type thridx    = i % work_group_size ;
+      //     Real_ptr vv_out = (Real_ptr) vv_int + 8*blk ;
+      //     INTSC_HEXHEX_BODY;
+      //   });
+      // });
 
-      qu->submit([&] (sycl::handler& h) {
-        h.parallel_for(sycl::nd_range<1>(gsize_fixup, work_group_size),
-                       [=] (sycl::nd_item<1> item ) {
+      // qu->submit([&] (sycl::handler& h) {
+      //   h.parallel_for(sycl::nd_range<1>(gsize_fixup, work_group_size),
+      //                  [=] (sycl::nd_item<1> item ) {
 
-          Index_type i = item.get_global_id(0) + ibegin;
-          Int_type ith     = i ;
-          FIXUP_VV_BODY ;
-        });
-      });
+      //     Index_type i = item.get_global_id(0) + ibegin;
+      //     Int_type ith     = i ;
+      //     FIXUP_VV_BODY ;
+      //   });
+      // });
 
     }
     stopTimer();
@@ -96,24 +96,24 @@ void INTSC_HEXHEX::runSyclVariantImpl(VariantID vid)
     startTimer();
     for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
 
-      RAJA::forall< RAJA::sycl_exec<work_group_size, true /*async*/> >( res,
-        RAJA::RangeSegment(ibegin, iend), [=] (Index_type i) {
+      // RAJA::forall< RAJA::sycl_exec<work_group_size, true /*async*/> >( res,
+      //   RAJA::RangeSegment(ibegin, iend), [=] (Index_type i) {
 
-        sycl::local_accessor<Real_type,1> vv_reduce(sycl::range<1>(16), h ) ;
+      //   sycl::local_accessor<Real_type,1> vv_reduce(sycl::range<1>(16), h ) ;
 
-        Index_type ith     = i ;
-        Index_type blksize = work_group_size ;
-        Index_type blk     = i / blksize ;
-        Index_type thridx  = i % work_group_size ;
-        Real_ptr vv_out = (Real_ptr) vv_int + 8*blk ;
-        INTSC_HEXHEX_BODY;
-      });
+      //   Index_type ith     = i ;
+      //   Index_type blksize = work_group_size ;
+      //   Index_type blk     = i / blksize ;
+      //   Index_type thridx  = i % work_group_size ;
+      //   Real_ptr vv_out = (Real_ptr) vv_int + 8*blk ;
+      //   INTSC_HEXHEX_BODY;
+      // });
 
-      RAJA::forall< RAJA::sycl_exec<work_group_size, true /*async*/> >( res,
-        RAJA::RangeSegment(ibegin, iend_fixup), [=] (Index_type i) {
-        Int_type ith     = i ;
-        FIXUP_VV_BODY ;
-        }
+      // RAJA::forall< RAJA::sycl_exec<work_group_size, true /*async*/> >( res,
+      //   RAJA::RangeSegment(ibegin, iend_fixup), [=] (Index_type i) {
+      //   Int_type ith     = i ;
+      //   FIXUP_VV_BODY ;
+      //   }
 
     }
     stopTimer();
