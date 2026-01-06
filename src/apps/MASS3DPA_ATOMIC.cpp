37656205c8dbf0a06c55f7691c9e03f8bba7c9c7
Arturo Vargas <vargas45@llnl.gov>
2026-01-06 06:31:43 -0800

    fix target problem setup

--- src/apps/MASS3DPA_ATOMIC.cpp
+++ src/apps/MASS3DPA_ATOMIC.cpp
@@ -21,22 +21,22 @@ MASS3DPA_ATOMIC::MASS3DPA_ATOMIC(const RunParams &params)
     : KernelBase(rajaperf::Apps_MASS3DPA_ATOMIC, params) {
 
   m_DOF_default = 1000000;
+  setDefaultProblemSize(m_DOF_default);
 
-  m_P = mpa3d_at::D1D - 1; // polynomial order
+  // polynomial order
+  m_P = mpa3d_at::D1D - 1;
 
-  m_NE = m_DOF_default/pow(m_P,3); //approximate how many elements we need
+  // approximate how many elements we need
+  m_NE = std::max(static_cast<Index_type>(getTargetProblemSize() / pow(m_P, 3)),
+                  Index_type(1));
 
-  //How does this work??
-  //getTargetProblemSize();
-  //m_NE = std::max(static_cast<Index_type>( (getTargetProblemSize() +   , Index_type(1));
-
-  //Construct the mesh
+  // Construct the mesh
   m_Nx = static_cast<int>(std::cbrt(m_NE));
   m_Ny = m_Nx;
   m_Nz = m_Ny;
   m_NE = m_Nx * m_Ny * m_Nz;
 
-  //compute true number of dofs
+  // compute true number of dofs
   m_Tot_Dofs = (m_Nx * m_P + 1) * (m_Ny * m_P + 1) * (m_Nz * m_P + 1);
 
   setDefaultProblemSize(m_Tot_Dofs);
@@ -47,10 +47,13 @@ MASS3DPA_ATOMIC::MASS3DPA_ATOMIC(const RunParams &params)
   setItsPerRep(m_NE * mpa3d_at::D1D * mpa3d_at::D1D);
   setKernelsPerRep(1);
 
-  setBytesReadPerRep(2 * sizeof(Real_type) * mpa3d_at::Q1D * mpa3d_at::D1D + // B, Bt
-                     1 * sizeof(Index_type) * mpa3d_at::D1D * mpa3d_at::D1D * mpa3d_at::D1D * m_NE + //ElemToDoF
+  setBytesReadPerRep(2 * sizeof(Real_type) * mpa3d_at::Q1D *
+                         mpa3d_at::D1D + // B, Bt
+                     1 * sizeof(Index_type) * mpa3d_at::D1D * mpa3d_at::D1D *
+                         mpa3d_at::D1D * m_NE +           // ElemToDoF
                      1 * sizeof(Real_type) * m_Tot_Dofs + // X
-                     1 * sizeof(Real_type) * mpa3d_at::Q1D * mpa3d_at::Q1D * mpa3d_at::Q1D * m_NE); // D
+                     1 * sizeof(Real_type) * mpa3d_at::Q1D * mpa3d_at::Q1D *
+                         mpa3d_at::Q1D * m_NE); // D
 
   setBytesWrittenPerRep(1 * sizeof(Real_type) * mpa3d_at::D1D * mpa3d_at::D1D *
                         mpa3d_at::D1D * m_NE); // Y
@@ -77,7 +80,7 @@ MASS3DPA_ATOMIC::MASS3DPA_ATOMIC(const RunParams &params)
   addVariantTunings();
 }
 
-MASS3DPA_ATOMIC::~MASS3DPA_ATOMIC() { }
+MASS3DPA_ATOMIC::~MASS3DPA_ATOMIC() {}
 
 void MASS3DPA_ATOMIC::setUp(VariantID vid,
                             size_t RAJAPERF_UNUSED_ARG(tune_idx)) {
@@ -98,7 +101,6 @@ void MASS3DPA_ATOMIC::setUp(VariantID vid,
 
   auto a_elemToDoF = allocDataForInit(m_ElemToDoF, total_size, vid);
   buildElemToDofTable(m_Nx, m_Ny, m_Nz, m_P, m_ElemToDoF);
-
 }
 
 void MASS3DPA_ATOMIC::updateChecksum(VariantID vid, size_t tune_idx) {
