bff2079cf82f0a79f12ab40514d0c51003ccca56
Samraat Gupta <gupta22@llnl.gov>
2026-07-16 10:58:56 -0700

    removed configs related to HEXHEX_NEW

--- src/common/RAJAPerfSuite.cpp
+++ src/common/RAJAPerfSuite.cpp
@@ -91,7 +91,6 @@
 #include "apps/FEMSWEEP.hpp"
 #include "apps/FIR.hpp"
 #include "apps/INTSC_HEXHEX.hpp"
-#include "apps/INTSC_HEXHEX_NEW.hpp"
 #include "apps/INTSC_HEXRECT.hpp"
 #include "apps/LTIMES.hpp"
 #include "apps/LTIMES_NOVIEW.hpp"
@@ -252,7 +251,6 @@ static const std::string KernelNames [] =
   std::string("Apps_FEMSWEEP"),
   std::string("Apps_FIR"),
   std::string("Apps_INTSC_HEXHEX"),
-  std::string("Apps_INTSC_HEXHEX_NEW"),
   std::string("Apps_INTSC_HEXRECT"),
   std::string("Apps_LTIMES"),
   std::string("Apps_LTIMES_NOVIEW"),
@@ -1191,10 +1189,6 @@ KernelBase* getKernelObject(KernelID kid,
     }
     case Apps_INTSC_HEXHEX : {
        kernel = new apps::INTSC_HEXHEX(run_params);
-       break;
-    }
-    case Apps_INTSC_HEXHEX_NEW : {
-       kernel = new apps::INTSC_HEXHEX_NEW(run_params);
        break;
     }
     case Apps_INTSC_HEXRECT : {
