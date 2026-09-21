341ab94c3d1f30ca942101bfbc3b30246ed18101
Robert Chen <chen59@llnl.gov>
2026-09-21 16:02:34 -0700

    Update CUB plus to 13.

--- src/algorithm/REDUCE_SUM-Hip.cpp
+++ src/algorithm/REDUCE_SUM-Hip.cpp
@@ -19,6 +19,7 @@
 #elif defined(__CUDACC__)
 #include "cub/device/device_reduce.cuh"
 #include "cub/util_allocator.cuh"
+#include <cuda/std/functional>
 #endif
 
 #include "common/HipDataUtils.hpp"
@@ -58,24 +59,24 @@ struct StreamInsertHelper<::rocprim::plus<R> const&>
 };
 #elif defined(__CUDACC__)
 template<>
-struct StreamInsertHelper<::cub::Sum&>
+struct StreamInsertHelper<::cuda::std::plus<>&>
 {
-  ::cub::Sum& m_val;
+  ::cuda::std::plus<>& m_val;
 
   std::ostream& operator()(std::ostream& str) const
   {
-    return str << "::cub::Sum";
+    return str << "::cuda::std::plus<>";
   }
 };
 ///
 template<>
-struct StreamInsertHelper<::cub::Sum const&>
+struct StreamInsertHelper<::cuda::std::plus<> const&>
 {
-  ::cub::Sum const& m_val;
+  ::cuda::std::plus<> const& m_val;
 
   std::ostream& operator()(std::ostream& str) const
   {
-    return str << "::cub::Sum";
+    return str << "::cuda::std::plus<>";
   }
 };
 #endif
@@ -154,7 +155,7 @@ void REDUCE_SUM::runHipVariantRocprim(VariantID vid)
         x+ibegin,
         sum,
         len,
-        ::cub::Sum(),
+        ::cuda::std::plus<>(),
         m_sum_init,
         stream);
 #endif
@@ -186,7 +187,7 @@ void REDUCE_SUM::runHipVariantRocprim(VariantID vid)
           x+ibegin,
           sum,
           len,
-          ::cub::Sum(),
+          ::cuda::std::plus<>(),
           m_sum_init,
           stream);
 #endif
